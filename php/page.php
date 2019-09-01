1faf994a7627bf8f84b4501e6b3cec22c32fef9e
SayHaHei <1041711058@qq.com>
2019-09-01 22:29:37 +0800

    php基础

---
+++ php/page.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>php 页面</title>
+</head>
+
+<body>
+    <div>welcome to php page</div>
+    <?php
+        //所有的php相关的代码都要写到这里
+        //echo的作用就是向页面中输出字符串
+        echo '<div>Hello world!</div>';
+    ?>
+</body>
+
+</html>
